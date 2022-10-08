1d41eed8035ea41bf92ff0cf6591d213a9138a8b
Karen Lara <karenm.larag@uqvirtual.edu.co>
2022-10-07 22:17:46 -0500

    Add code review fixes

--- reto.exs
+++ reto.exs
@@ -1,23 +1,20 @@
-defmodule Recursion do
-  def print_multiple_times(msg, cantidad, cont, limit_inf, limit_sup) when cont <= limit_sup do
+defmodule SeriesUtils do
+  def count_numbers_series(msg, amount, cont, lower_limit, upper_limit) when cont <= upper_limit do
     msg = msg <> "#{cont}"
-    numero = String.to_integer(msg)
-    if cont >= limit_inf do
-      IO.puts(numero)
-      cond do
-      rem(numero,3) == 0 -> print_multiple_times(msg, cantidad + 1, cont + 1, limit_inf, limit_sup)
-      true -> print_multiple_times(msg, cantidad, cont + 1, limit_inf, limit_sup)
-      end
+    number = String.to_integer(msg)
+    if cont >= lower_limit do
+      IO.puts(number)
+      count_numbers_series(msg, amount + (rem(number,3) == 0 && 1 || 0) , cont + 1, lower_limit, upper_limit)
     else
-      print_multiple_times(msg, cantidad, cont + 1, limit_inf, limit_sup)
+      count_numbers_series(msg,amount, cont + 1, lower_limit, upper_limit)
     end
   end
 
-  def print_multiple_times(_msg,cantidad, cont, _limit_inf, limit_sup ) when cont > limit_sup do
-      cantidad
+  def count_numbers_series(_msg, amount, cont, _lower_limit, upper_limit) when cont > upper_limit do
+      amount
   end
 end
 
-numeros =  Recursion.print_multiple_times("",0, 1, 3, 5)
-IO.puts("Resultado: ")
-IO.puts(numeros)
+result =  SeriesUtils.count_numbers_series("",0, 1, 3, 5)
+IO.puts("Result: ")
+IO.puts(result)
